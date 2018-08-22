65ee61bdc1498cca362d062869caf390558695a5
zsg <zhusg@tuyasmart.com>
2018-08-22 18:06:22 +0800

    增加退出登录

--- TuyaSmartMeshSdkDemo/src/main/java/com/smart/tuya/meshdemo/activity/MeshDemoActivity.java
+++ TuyaSmartMeshSdkDemo/src/main/java/com/smart/tuya/meshdemo/activity/MeshDemoActivity.java
@@ -12,6 +12,7 @@ import com.smart.tuya.meshdemo.presenter.FamilyPresenter;
 import com.smart.tuya.meshdemo.presenter.LoginPresenter;
 import com.smart.tuya.meshdemo.presenter.MeshPresenter;
 import com.smart.tuya.meshdemo.view.IMeshDemoView;
+import com.tuya.smart.android.user.api.ILogoutCallback;
 import com.tuya.smart.home.sdk.TuyaHomeSdk;
 
 import static com.smart.tuya.meshdemo.presenter.MeshPresenter.GPS_REQUEST_CODE;
@@ -147,5 +148,9 @@ public class MeshDemoActivity extends AppCompatActivity implements IMeshDemoView
         } else {
             Toast.makeText(this, "请先初始化家庭", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void doLogout(View view) {
+        mLoginPresenter.logout();
     }
 }
